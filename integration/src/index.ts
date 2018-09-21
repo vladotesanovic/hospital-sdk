ebb7422f6ec72126926770f770716926ad463985
Vlado Tešanović <vlado.tesanovic@gmail.com>
2018-09-22 00:43:56 +0200

    integration

--- integration/src/index.ts
+++ integration/src/index.ts
@@ -1,3 +1,7 @@
 import { Doctor } from "@hospital-sdk/doctor";
+import { Patient } from "@hospital-sdk/patient";
+import { Scheduler } from "@hospital-sdk/scheduler";
 
 console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
